e79ecbd784ab44082b7b2581bf9b745bdf61d045
andengarry <anden.garry@gmail.com>
2026-07-30 08:11:51 +0200

    scroll to top on page change enabled

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Kitchen from "./pages/services/Kitchen";
 import DoorsAndWindows from "./pages/services/DoorsAndWindows";
 import Roof from "./pages/services/Roof";
 import NewBuildAndRenovation from "./pages/services/NewBuildAndRenovation";
-
+import ScrollToTop from "./components/ScrollToTop";
 import { Navbar } from "./components/Navbar";
 import Footer from "./components/Footer";
 
@@ -18,28 +18,25 @@ function App() {
     return (
       <>
 
-  {/* Global navigation */}
-  <Navbar />
+        <ScrollToTop />
 
+        <Navbar />
 
-   {/* Sider */}
-          
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/services" element={<Gallery />} />
-      <Route path="/services/køkkenopsætning" element={<Kitchen />} />
-      <Route path="/services/døre-og-vinduer" element={<DoorsAndWindows />} />
-      <Route path="/services/tagrenovering" element={<Roof />} />
-      <Route path="/services/nybygning-og-tilbygning" element={<NewBuildAndRenovation />} />
-      <Route path="/galleri" element={<Gallery />} />
-      <Route path="/om-os" element={<About />} />
-      <Route path="/kontakt" element={<Contact />} />
-    </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/services" element={<Gallery />} />
+          <Route path="/services/køkkenopsætning" element={<Kitchen />} />
+          <Route path="/services/døre-og-vinduer" element={<DoorsAndWindows />} />
+          <Route path="/services/tagrenovering" element={<Roof />} />
+          <Route path="/services/nybygning-og-tilbygning" element={<NewBuildAndRenovation />} />
+          <Route path="/galleri" element={<Gallery />} />
+          <Route path="/om-os" element={<About />} />
+          <Route path="/kontakt" element={<Contact />} />
+        </Routes>
 
+        <Footer />
 
-    <Footer />
-
-        </>
+      </>
 
   );
 }
